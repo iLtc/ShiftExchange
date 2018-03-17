59b62542533dea1c58aaebf6a9b7a0b0a2b50053
iLtc <i@iltc.io>
2018-03-17 18:11:51 -0500

    Removed Name from Permissions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180317225835) do
+ActiveRecord::Schema.define(version: 20180317231046) do
 
   create_table "key_values", force: :cascade do |t|
     t.string "key"
@@ -31,7 +31,6 @@ ActiveRecord::Schema.define(version: 20180317225835) do
   end
 
   create_table "permissions", force: :cascade do |t|
-    t.string "name"
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
