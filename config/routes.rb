a57f32366f1e2809f2121f9b35a66d657f4e1145
iLtc <i@iltc.io>
2018-03-16 20:43:31 -0500

    Added Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'shifts#index'
+
+  resources :sessions
+  get '/sessions/callback/:platform', to: 'sessions#callback'
+
+  resources :shifts
+  post '/shifts/new', to: 'shifts#create'
 end
