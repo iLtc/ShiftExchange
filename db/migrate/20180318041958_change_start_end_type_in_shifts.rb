971fbf32c094d126e0fb32487547481d27547ef5
iLtc <i@iltc.io>
2018-03-17 23:39:14 -0500

    Remove Wrong Migrate

--- db/migrate/20180318041958_change_start_end_type_in_shifts.rb
+++
@@ -1,8 +0,0 @@
-class ChangeStartEndTypeInShifts < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :shifts, :start, :string
-    remove_column :shifts, :end, :string
-    add_column :shifts, :start, :time
-    add_column :shifts, :end, :time
-  end
-end
