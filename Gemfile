7fd37feaae194e04281918a75cd47418c31bf93b
iLtc <i@iltc.io>
2018-05-20 13:34:05 -0500

    Added Seeds for future test

--- Gemfile
+++ Gemfile
@@ -43,6 +43,8 @@ group :development, :test do
   gem 'selenium-webdriver'
   # Use sqlite3 as the database for Active Record
   gem 'sqlite3'
+
+  gem 'seed_dump'
 end
 
 group :development do
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Role::HABTM_Users.create!([
+  {role_id: 1, user_id: 1},
+  {role_id: 1, user_id: 2},
+  {role_id: 2, user_id: 3}
+])
+User::HABTM_Roles.create!([
+  {role_id: 1, user_id: 1},
+  {role_id: 1, user_id: 2},
+  {role_id: 2, user_id: 3}
+])
+LoginCredential.create!([
+  {credential: "tZ3DZfVBjOtTorr83ERrdLQJaRXLitNq", platform: "Demo", user_id: 1},
+  {credential: "67GcaiPYyDKFcC6MY4fFg5rXUvptDyU6", platform: "Demo", user_id: 2},
+  {credential: "krYtpOeIQSbMgMan_DmtO0D_YaUddkI8", platform: "Demo", user_id: 3}
+])
+Role.create!([
+  {title: "Student"},
+  {title: "Manager"}
+])
+User.create!([
+  {first_name: "Demo Student", last_name: "1", prefer_name: nil, credits: 0, level: 0},
+  {first_name: "Demo Student", last_name: "2", prefer_name: nil, credits: 0, level: 0},
+  {first_name: "Demo", last_name: "Manager", prefer_name: nil, credits: 0, level: 0}
+])
