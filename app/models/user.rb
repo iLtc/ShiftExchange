e298b5adafa4e8bd8634d2292b539e9ac7718209
iLtc <i@iltc.io>
2018-05-28 14:41:09 -0500

    Added a new relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   has_many :login_credentials
   has_and_belongs_to_many :roles
   has_many :permissions, through: :roles
+  has_and_belongs_to_many :shifts
 
   def show_name
     if prefer_name
