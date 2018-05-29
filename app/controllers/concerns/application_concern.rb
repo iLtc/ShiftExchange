4579d0b0515fa4626ef52f575ad9f5d12cc238d8
iLtc <i@iltc.io>
2018-05-28 20:22:24 -0500

    Added Comment and Log Helper

--- app/controllers/concerns/application_concern.rb
+++ app/controllers/concerns/application_concern.rb
@@ -8,4 +8,16 @@ module ApplicationConcern
   def get_weekday(date)
     Date::DAYNAMES[date.wday]
   end
+
+  def add_comment(user, content, target)
+    if target.class == Shift
+      Comment.create(user_id: user.id, content: content, target_type: 'shift', target_id: target.id)
+    end
+  end
+
+  def add_log(user, action, target, data = nil)
+    if target.class == Shift
+      Log.create(user_id: user.id, action: action, target_type: 'shift', target_id: target.id, data:data.to_json)
+    end
+  end
 end
